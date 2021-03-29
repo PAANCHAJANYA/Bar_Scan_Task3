d9b4a938aa4d03e0f578cf43b0e9d10d53a1bf19
KRISHNA PAANCHAJANYA <krishnapaanchajanya1966@gmail.com>
2021-03-29 18:34:14 +0530

    Add project files.

---
+++ Bar_Scan_Task3/Bar_Scan_Task3/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Bar_Scan_Task3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ZXingScannerView_OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                scanResultText.Text = result.Text;
+            });
+        }
+    }
+}
